9079aeb87a783679b911c54c6746c8a08e8eb5b9
Alaa-eddine <alaa.eddine@outlook.com>
2013-06-09 21:09:59 +0200

    removed not working example

--- examples/1-websocket-only/client.js
+++
@@ -1,8 +0,0 @@
-﻿var EurecaClient = require('../../').EurecaClient;
-var client = new EurecaClient({ uri: 'http://localhost:8000/', prefix: 'eureca.io', retry: 3 });
-
-client.ready(function (proxy) {
-
-    proxy.hello();
-    
-});
