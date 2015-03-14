70e39c4de965e4a13a19ea57954d589e88187bec
Alaa-eddine <alaa.eddine@outlook.com>
2015-03-14 16:38:28 +0100

    Added webrtc example

---
+++ examples/7-webrtc/client.js
@@ -0,0 +1,17 @@
+﻿var Eureca = require('../../');
+
+var client = new Eureca.Client({ uri: 'http://localhost:8000/', transport: 'webrtc', reliable: false, maxRetransmits: 3, ordered: true });
+
+client.exports.hello = function() {
+    console.log('hello from server');
+}
+
+client.exports.sub = function(a, b) {
+	return a - b;
+}
+client.ready(function (proxy) {
+
+    proxy.hello().onReady(function(r) {
+	    console.log('returned ', r);
+	});
+});
---
+++ examples/7-webrtc/server.js
@@ -0,0 +1,58 @@
+﻿var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+var Eureca = require('../../');
+
+var eurecaServer = new Eureca.Server({transport:'webrtc', allow:['hello', 'sub']});
+
+eurecaServer.attach(server);
+
+//functions under "exports" namespace will
+//be exposed to client side
+eurecaServer.exports.hello = function () {
+    var client = eurecaServer.getClient(this.connection.id);
+    client.hello();
+    console.log('Hello from client');
+	return ('hello return');
+}
+
+
+eurecaServer.onConnect(function (conn) {
+    
+    console.log('new Client');
+    var client = eurecaServer.getClient(conn.id);
+    
+
+    
+    client.sub(10, 4).onReady(function (r) {
+        console.log('> 10 - 4 = ', r);
+    });
+    
+});
+
+
+
+
+server.on('request', function (request, response) {
+    var i;
+    
+    if (request.method === 'GET') {
+        
+        if (request.url.split('?')[0] === '/') {
+            var filename = __dirname + '/index.html';
+            fs.readFile(filename, function (err, data) {
+                var text = data.toString();
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(text);
+                response.end();
+            });
+        }
+    }
+
+});
+
+
+console.log('\033[96mlistening on localhost:8000 \033[39m');
+server.listen(8000);
